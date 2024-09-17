bae02275ca27f44cb169d44a9a041f33e7405004
Sobhan Zadehali <99137872+sobhanzadehali@users.noreply.github.com>
2024-09-17 05:50:51 -0700

    add-restframework-authtoken

--- core/core/settings.py
+++ core/core/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',  # account app
-
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
