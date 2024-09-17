960e354f7010cdceb5f8d375dc2865b10a9512f4
Sobhan Zadehali <99137872+sobhanzadehali@users.noreply.github.com>
2024-09-17 05:06:33 -0700

    add-custom-user-model
    + User model
    + setting -> default user model
    + register user model admin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from core.accounts.models import User
 
 # Register your models here.
+
+admin.site.register(User, UserAdmin)
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    is_verified = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
